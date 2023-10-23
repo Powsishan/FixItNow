377a1102dbd59143d3614d2ff4ee6507a14884d1
Powsikan <powsi.shan@gmail.com>
2023-10-24 01:20:51 +0530

    secound

--- login-app-backend/server.js
+++ login-app-backend/server.js
@@ -2,21 +2,26 @@ const express = require('express');
 const mysql = require('mysql');
 const bodyParser = require('body-parser');
 const session = require('express-session');
-const cors = require('cors'); // Import the 'cors' middleware.
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
+const bcrypt = require('bcrypt');
 
 const app = express();
 const port = 3001;
 
 // Middleware
 app.use(bodyParser.json());
-
 app.use(cors({
-  origin: 'http://localhost:3000',  // replace with the domain of your frontend
+  origin: 'http://localhost:3000',
   credentials: true,
 }));
-app.use(session({ secret: 'mySuperSecretKey', cookie: { maxAge: 60000 }}));
-// Use the 'cors' middleware to enable cross-origin requests.
-// MySQL connection setup
+app.use(cookieParser());
+
+app.use(session({
+  secret: 'mySuperSecretKey',
+  cookie: { maxAge: 60000 },
+}));
+
 const db = mysql.createConnection({
   host: 'localhost',
   user: 'root',
@@ -32,12 +37,33 @@ db.connect((err) => {
   }
 });
 
+// Registration route
+app.post('/register', async (req, res) => {
+  const { username, email, password } = req.body;
 
+  try {
+    // Hash the password
+    const hashedPassword = await bcrypt.hash(password, 10);
 
+    // Insert the new user into the database
+    const insertUserQuery = 'INSERT INTO serviceproviderprofile (username, email, password) VALUES (?, ?, ?)';
+    db.query(insertUserQuery, [username, email, hashedPassword], (err) => {
+      if (err) {
+        console.error('MySQL error:', err);
+        return res.status(500).json({ message: 'Internal server error' });
+      }
 
+      // Registration successful
+      return res.status(200).json({ message: 'User registered' });
+    });
+  } catch (error) {
+    console.error('Error hashing the password:', error);
+    return res.status(500).json({ message: 'Internal server error' });
+  }
+});
 
-// Route for login
-app.post('/login', (req, res) => {
+// Login route
+app.post('/login', async (req, res) => {
   const { username, password } = req.body;
 
   // Validate that username and password are provided
@@ -47,7 +73,7 @@ app.post('/login', (req, res) => {
 
   // Query the database to check if the user exists
   const query = 'SELECT * FROM serviceproviderprofile WHERE username = ?';
-  db.query(query, [username], (err, results) => {
+  db.query(query, [username], async (err, results) => {
     if (err) {
       console.error('MySQL error:', err);
       return res.status(500).json({ message: 'Internal server error' });
@@ -60,35 +86,35 @@ app.post('/login', (req, res) => {
 
     const user = results[0];
 
-    // Check if the provided password matches the stored password (in a real app, you should hash and compare passwords)
-    if (password === user.password) {
-      req.session.username = user.username;
-      console.log('Logged in user: ', req.session.username);
-      return res.status(200).json({ message: 'Login successful' });
+    // Check if the provided password matches the stored password
+    try {
+      const passwordMatch = await bcrypt.compare(password, user.password);
+      if (passwordMatch) {
+        req.session.username = user.username;
+        console.log('Logged in user:', req.session.username);
+        return res.status(200).json({ message: 'Login successful' });
+      } else {
+        return res.status(401).json({ message: 'Invalid credentials' });
+      }
+    } catch (error) {
+      console.error('Error comparing passwords:', error);
+      return res.status(500).json({ message: 'Internal server error' });
     }
-  
-    // If the password doesn't match, send an error response
-    return res.status(401).json({ message: 'Invalid credentials' });
   });
 });
 
-
-
-
-//get the userdata 
+// Get user profile data
 app.get('/profile-data', (req, res) => {
-  
   const username = req.session.username;
-  console.log('Requesting profile data for user: ', req.session.username);
+  console.log('Dashboard user:', req.session.username);
   if (!username) {
     return res.status(401).json({ error: 'Unauthorized' });
   }
-  console.log('Requesting profile data for user: ', req.session.username);
 
   const query = 'SELECT * FROM ServiceProviderProfile WHERE Username = ?';
   db.query(query, [username], (err, results) => {
     if (err) {
-      console.error('Error fetching profile data: ', err);
+      console.error('Error fetching profile data:', err);
       res.status(500).json({ error: 'Internal Server Error' });
       return;
     }
@@ -102,61 +128,12 @@ app.get('/profile-data', (req, res) => {
   });
 });
 
-
-// Route for registration
-app.post('/register', (req, res) => {
-  const { username, email, password } = req.body;
-
-  // Validate that all required fields are provided
-  if (!username || !email || !password) {
-    return res.status(400).json({ message: 'All fields are required.' });
-  }
-
-  // Check if a user with the same username already exists
-  const checkUsernameQuery = 'SELECT * FROM users WHERE username = ?';
-  db.query(checkUsernameQuery, [username], (err, usernameResults) => {
-    if (err) {
-      console.error('MySQL error:', err);
-      return res.status(500).json({ message: 'Internal server error' });
-    }
-
-    if (usernameResults.length > 0) {
-      return res.status(409).json({ message: 'Username is already in use.' });
-    }
-
-    // Check if a user with the same email already exists
-    const checkEmailQuery = 'SELECT * FROM users WHERE email = ?';
-    db.query(checkEmailQuery, [email], (err, emailResults) => {
-      if (err) {
-        console.error('MySQL error:', err);
-        return res.status(500).json({ message: 'Internal server error' });
-      }
-
-      if (emailResults.length > 0) {
-        return res.status(409).json({ message: 'Email is already in use. Please log in.' });
-      }
-
-      // Insert the new user into the database (in a real app, you should hash the password)
-      const insertUserQuery = 'INSERT INTO users (username, email, password) VALUES (?, ?, ?)';
-      db.query(insertUserQuery, [username, email, password], (err) => {
-        if (err) {
-          console.error('MySQL error:', err);
-          return res.status(500).json({ message: 'Internal server error' });
-        }
-
-        // Registration successful
-        return res.status(200).json({ message: 'User registered' });
-      });
-    });
-  });
-});
-
-
-
 app.get('/serviceproviderprofile', (req, res) => {
-  const query = 'SELECT firstName, lastName,Description, img, JobTitle FROM serviceproviderprofile';
+  const query = 'SELECT firstName, lastName, Description, img, JobTitle FROM serviceproviderprofile';
   db.query(query, (err, result) => {
-    if (err) { return res.status(500).json({ message: 'Internal Server Error' });}
+    if (err) {
+      return res.status(500).json({ message: 'Internal Server Error' });
+    }
     res.status(200).json(result);
   });
 });

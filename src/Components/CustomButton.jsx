e4de894324caf84b8afc08bf7b490e1ff7b1204a
Nk008 <nareshkumar.shanmugathas@gmail.com>
2023-10-30 19:09:35 +0530

    nar

--- src/Components/CustomButton.jsx
+++ src/Components/CustomButton.jsx
@@ -8,6 +8,8 @@ const CustomButton = ({
   heroBtn,
   guideBtn,
   getStartedBtn,
+  tesr,
+  
 }) => {
   const CustomButton = styled(Button)(({ theme }) => ({
     backgroundColor: backgroundColor,
